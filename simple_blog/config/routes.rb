5df154b4d88424701d647d93e040e6ced20bedd8
sean <wcolts2000@gmail.com>
2020-03-07 01:00:29 -0800

    added bootstrap navbar

--- simple_blog/config/routes.rb
+++ simple_blog/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   #creates all the crud routes for specified controller
   resources :posts
